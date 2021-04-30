a6591be05b08da4dd30471009a46be4fe87839a5
Gavin Pereira <gavineditz@gmail.com>
2021-04-30 10:05:07 +0530

    dummy questions made longer and fixed hight issue

--- db/faq.js
+++ db/faq.js
@@ -6,27 +6,29 @@ export var faqs = [
     },
     {
         'id': 2,
-        'q': 'Question',
-        'a': 'Answer'
+        'q': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+        'a': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.'
     },
     {
         'id': 3,
-        'q': 'Question',
-        'a': 'Answer'
+        'q': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod?',
+        'a': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
     },
     {
         'id': 4,
-        'q': 'Question',
-        'a': 'Answer'
-    }, {
+        'q': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore?',
+        'a': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
         'id': 5,
-        'q': 'Question',
-        'a': 'Answer'
+        'q': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
+        'a': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.'
     },
     {
         'id': 6,
-        'q': 'Question',
-        'a': 'Answer'
+        'q': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod?',
+        'a': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
     },
+
 
 ]
